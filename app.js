8f345578e4f8c31d1e531d8f878236620c6901d3
velvas45 <helmiagustiawan905@gmail.com>
2021-01-14 01:18:36 +0700

    menambahkan comment

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const consonantHTML = document.getElementById('consonant');
 
 // function vowelAndConsonan
 const vowelAndConsonan = () => {
+    // regex untuk mencari huruf vocal
     const vowelRegex = /[aiueo]/
+    // algoritma
+    // masukkan kalimat -> diubah ke lowercase
     const textValue = inputText.value.toLowerCase();
+    // ganti kalimat menjadi tidak ada spasi/whitespace
     const textValueNoSpace = textValue.replace(/\s/g, '');
+    // ubah kalimat menjadi sebuah huruf -> bentuknya array
     const words = textValueNoSpace.split('');
-    const filterVowel = words.filter(word => word.match(vowelRegex)).sort().join();
+    // filter huruf tersebut jika ada consonan maka masuk ke consonan, jika huruf vocal masuk ke vocal sekalian sorting untuk kedua huruf tadi => berubah menjadi string karena join. 
+    const filterVowel = words.filter(word => word.match(vowelRegex)).sort().join(); 
     const filterConsonant = words.filter(word => !word.match(vowelRegex)).sort().join();
+    // return ke html / console.log
     vowelHTML.innerHTML = filterVowel;
     consonantHTML.innerHTML = filterConsonant;
    
